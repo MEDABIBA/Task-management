2a9a8d05adf68f97f218bfdf1ac3b757b7ccab67
Viacheslav <s.l.a.v.a.2007007@gmail.com>
2025-08-14 15:37:11 +0200

    .js on .tsx

---
+++ my-app/src/components/Card/Card.tsx
@@ -0,0 +1,12 @@
+const Card = ({ children, classes = "" }) => {
+  return (
+    <div
+      className={`card ${
+        Array.isArray(classes) ? classes.join(" ") : classes
+      }`}>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
